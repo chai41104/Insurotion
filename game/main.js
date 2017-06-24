78608054dce9ad9660171d481b891f3dfb2baace
chai41104 <chai41104@hotmail.com>
2017-06-24 19:39:31 +0100

    add crop

--- game/main.js
+++ game/main.js
@@ -5,16 +5,31 @@ var fields = [];
 var cropNames = ["Rice", "Corn", "Potato", "Wheat", "Vegetables", "Coffee", "Bean"];
 var cost = [];
 
+var cropSelect;
+var fieldSelect;
+
+var prices = {};
 function startGame() {
 	money = 10000;
 	year = 1;
 	fields = [];
+	prices = {};
 
 	resetField();
 	updatePage();
 
 	for(var i = 0; i < cropNames.length; ++i) {
 		cost[i] = 100;
+	}
+
+	addData();
+}
+
+function addData() {
+	var tableEle = $("#tableCrop");
+	tableEle.empty();
+	for(var i = 0; i < cropNames.length; ++i) {
+		tableEle.append("<tr cropNameId='" +cropNames[i]+ "'><td cropNameId='" +cropNames[i]+ "'>" +cropNames[i]+ "</td><td cropNameId='" +cropNames[i]+ "'>" +cost[i]+ "</td></tr>");
 	}
 }
 
@@ -26,6 +41,12 @@ function updatePage() {
 function growCorp(i, cropName) {
 	fields[i] = cropName;
 	money -= cost[cropNames.indexOf(cropName)];
+	var ele = $("#fieldCropId"+i).children()[0];
+	ele.className = 'info-box bg-green';
+	ele.childNodes[1].childNodes[0].innerHTML = cropName;
+
+
+	updatePage();
 }
 
 function updateNewTurn() {
@@ -43,17 +64,18 @@ function resetField() {
 }
 
 function newFieldHTML(i) {
-	text = '<div class="col-md-3 col-sm-6 col-xs-12" fieldId="'+i+'">';
+	text = '<div class="col-md-3 col-sm-6 col-xs-12" fieldId="'+i+'" id="fieldCropId'+i+'">';
 	text += '<div class="info-box bg-yellow" fieldId="'+i+'" id="field'+i+'">';
 	text += '<span class="info-box-icon" fieldId="'+i+'"><i class="fa fa-bookmark-o" fieldId="'+i+'"></i></span>';
 	text += '<div class="info-box-content" fieldId="'+i+'">';
-	text += '<span class="info-box-text" fieldId="'+i+'">Bookmarks</span>';
+	text += '<span class="info-box-text" fieldId="'+i+'">empty</span>';
 	text += '</div></div></div>';
 	return text;
 }
 
 function selectCrop(i) {
-	
+	$('#selectCropModal').modal('toggle');
+	fieldSelect = i;
 }
 
 $( "body" ).click(function( event ) {
@@ -61,8 +83,18 @@ $( "body" ).click(function( event ) {
   if(elem.hasAttribute( 'fieldid' )) {
   	selectCrop(elem.getAttribute( 'fieldid' ));
   }
+  else if(elem.hasAttribute( 'cropNameId' )) {
+  	cropselect = elem.getAttribute( 'cropNameId' );
+  	growCorp(fieldSelect, cropselect);
+  	$('#selectCropModal').modal('toggle');
+  }
+  else if(elem.hasAttribute( 'endYear' )) {
+  	console.log("trigger");
+  	$('#endYearModal').modal('toggle');
+  }
 });
 
 $( document ).ready(function() {
 	startGame();
+	$("#cropNameTable").DataTable();
 });
